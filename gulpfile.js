d712f4c14eedd67e982242278973c2115d237960
kaidez <kai.gittens@gmail.com>
2014-09-16 15:30:10 -0400

    remove useless brackets

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 
 // Shell out Haml build command
-gulp.task('haml', shell.task([
+gulp.task('haml', shell.task(
   'haml index.haml build/index.html'
-]))
+))
 
 // Test to see if gulp is installed
 gulp.task('default', function() {
