9c04f513299a014944f2238412826557623d4d8e
kaidez <kai.gittens@gmail.com>
2014-09-16 15:20:06 -0400

    setup haml build out wilh gulp-shell

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,9 @@
 var gulp = require('gulp');
+var shell = require('gulp-shell');
+
+gulp.task('haml', shell.task([
+  'haml index.haml build/index.html'
+]))
 
 // Test to see if gulp is installed
 gulp.task('default', function() {
