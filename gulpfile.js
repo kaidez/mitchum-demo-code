11ee0ae31ff2ce1da65da83740f1faaa843886e1
Kai Gittens <kai.gittens@gmail.com>
2014-09-16 22:07:24 -0400

    add gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
+var watch = require('gulp-watch');
 
 // Shell out Haml build command
 gulp.task('haml', shell.task(
   'haml index.haml build/index.html'
 ))
+
+//watch
+gulp.task('watch', function() {
+
+  // If 'index.haml', changes, build out 'index.html'
+  gulp.watch('index.haml', ['haml']);
+});
 
 // Test to see if gulp is installed
 gulp.task('default', function() {
