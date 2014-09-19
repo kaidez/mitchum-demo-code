dd74d9d1d1f232a9d99a90a902152c4b43ea3008
Kai Gittens <kai.gittens@gmail.com>
2014-09-19 09:44:28 -0400

    start configuring grunt bowercopy tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,35 +3,43 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    wiredep: {
+    bowercopy: {
 
-      target: {
-
-        // Point to the files that should be updated when
-        // you run `grunt wiredep`
-        src: [
-          'app/views/**/*.html',   // .html support...
-          'app/views/**/*.jade',   // .jade support...
-          'app/styles/main.scss',  // .scss & .sass support...
-          'app/config.yml'         // and .yml & .yaml support box!
-        ],
-
-        // Optional:
-        // ---------
+      // don't send messages to saying that Bower components aren't
+      // configured...ignore them instead
+      options:{
+        ignore: ['less','gulp','normalize-less']
+      },
+      css_build: {
+        // copy over cs files
         options: {
-          cwd: '',
-          dependencies: true,
-          devDependencies: false,
-          exclude: [],
-          fileTypes: {},
-          ignorePath: '',
-          overrides: {}
+          destPrefix: 'build/css'
+        },
+        files: {
+          'bootstrap.min.css': 'bootstrap/dist/css/bootstrap.min.css'
         }
-      }
-    }
+      }, // end css_build
+
+      // start task for copying over JS libraries
+      js_libs: {
+
+        // copy JS libraries over to "build/js/libs"
+        options: {
+          destPrefix: 'build/js/libs'
+        },
+
+        // libraries that need to be copied...listed in the order they
+        // would appear on "index.html"
+        files: {
+          'masonry.min.js': 'masonry/dist/masonry.pkgd.min.js',
+          'scrollNav.min.js': 'scrollNav/dist/jquery.scrollNav.min.js',
+          'jquery.min.js': 'jquery/jquery.min.js'
+        }
+      }, // end js_libslibs
+    } // end bowercopy
   });
 
-  grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks('grunt-bowercopy');
 
   // Default task(s).
   // grunt.registerTask('default', ['uglify']);
