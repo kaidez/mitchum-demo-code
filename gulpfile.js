71699fcdcd776b3ea1cfccad43d038d788965272
Kai Gittens <kai.gittens@gmail.com>
2014-09-19 11:28:39 -0400

    make grunt bowercopy run via gulp-grunt

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,17 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var watch = require('gulp-watch');
 var gulp = require('gulp');
-require('gulp-grunt')(gulp); 
+require('gulp-grunt')(gulp);
 
 // Shell out Haml build command
 gulp.task('haml', shell.task(
   'haml index.haml build/index.html'
 ));
+
+gulp.task('bc', function() {
+  // run complete grunt tasks
+  gulp.run('grunt-bowercopy');
+});
 
 //watch
 gulp.task('watch', function() {
